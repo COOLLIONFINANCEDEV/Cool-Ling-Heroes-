ff4e1a7d1231528d2454b0b0075c21abae2f2e41
sylla <s67986601@gmail.com>
2023-06-06 11:34:44 +0000

    FEATURE: Add formik on signup form

--- src/Components/Login/Register.tsx
+++ src/Components/Login/Register.tsx
@@ -1,11 +1,41 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
-
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+import { FormTextField } from "../Formik/FormTextField";
+import { LoadingButton } from "@mui/lab";
+import countriesList from "../../Seeds/Country";
 interface REGISTER {
   hanbleChange?: Function;
 }
 
 const Register: React.FC<REGISTER> = ({ hanbleChange }) => {
+  interface INITIALVALUES {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    phone: string;
+    prefixe: string;
+  }
+  const initialValues: INITIALVALUES = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+    prefixe: "",
+  };
+
+  const handleSubmit = (values: INITIALVALUES) => {
+    // same shape as initial values
+    console.log(values);
+  };
+
   return (
     <Box
       style={{
@@ -18,49 +48,111 @@ const Register: React.FC<REGISTER> = ({ hanbleChange }) => {
       }}
     >
       <Typography variant="h2">Sign UP</Typography>
-      <Typography  sx={{ marginBottom: "5vh" }}>
+      <Typography sx={{ marginBottom: "5vh" }}>
         Start investing or funding your projects today.
       </Typography>
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          rowGap: "20px",
-        }}
-        component="form"
+      <Formik
+        initialValues={initialValues}
+        validationSchema={yup.object().shape({
+          email: yup.string().email().required(),
+          phone: yup.string().matches(/^\d{8,12}$/, "Invalid phone number"),
+          password: yup.string().min(8).required(),
+          confirmPassword: yup
+            .string()
+            .oneOf([yup.ref("password"), "ll"], "Passwords must match")
+            .required(),
+        })}
+        onSubmit={handleSubmit}
       >
-        <TextField
-          label="Email"
-          type={"email"}
-          name={"email"}
-          id={"email"}
-          variant="outlined"
-          sx={{ width: "95%" }}
-        />
-        <TextField
-          label="Password"
-          type={"password"}
-          name={"password"}
-          id={"password"}
-          variant="outlined"
-          sx={{ width: "95%" }}
-        />
-        <TextField
-          label="Confirm Password"
-          type={"password"}
-          name="confirmPassword"
-          id="confirmPassword"
-          variant="outlined"
-          sx={{ width: "95%" }}
-        />
+        {({ isSubmitting }) => (
+          <Form
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              rowGap: "20px",
+            }}
+          >
+            <Field
+              label="Email"
+              type={"email"}
+              name={"email"}
+              sx={{ width: "95%" }}
+              component={FormTextField}
+            />
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              width={"95%"}
+            >
+              <Field
+                sx={{ width: "22%" }}
+                as={Select}
+                label={"code"}
+                name={"prefixe"}
+                select
+                component={FormTextField}
+              >
+                {countriesList.map((item) => {
+                  if (item.idd.root === undefined) {
+                    // eslint-disable-next-line array-callback-return
+                    return;
+                  }
+                  return (
+                    <MenuItem
+                      value={`${
+                        item?.idd?.root
+                      }${item?.idd?.suffixes?.join()} `}
+                      key={item.name.common}
+                    >
+                      {`${item?.idd?.root}${item?.idd?.suffixes
+                        ?.slice(0, 2)
+                        ?.join(` ${item?.idd?.root}`)} `}{" "}
+                      {item?.flag}
+                    </MenuItem>
+                  );
+                })}
+              </Field>
+              <Field
+                label="Phone number"
+                type={"phone"}
+                name={"phone"}
+                sx={{ width: "75%" }}
+                component={FormTextField}
+              />
+            </Stack>
+            <Field
+              label="Password"
+              type={"password"}
+              name={"password"}
+              sx={{ width: "95%" }}
+              component={FormTextField}
+            />
+            <Field
+              label="Confirm Password"
+              type={"password"}
+              name="confirmPassword"
+              sx={{ width: "95%" }}
+              component={FormTextField}
+            />
 
-        <Button variant="contained" sx={{ width: "95%" }} type="submit">
-          Sign up
-        </Button>
-      </Box>
+            <LoadingButton
+              variant="contained"
+              sx={{ width: "95%" }}
+              type="submit"
+              loading={isSubmitting}
+              loadingPosition="center"
+              loadingIndicator="...loading"
+              size="large"
+            >
+              Sign up
+            </LoadingButton>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
 };
